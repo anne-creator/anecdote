864e44e497e115a32956b70731ecad500b281464
Anne <anneliu49@gmail.com>
2023-11-01 12:09:57 -0400

    add put api server to AnecdoteTable route.js

--- backend/src/app/api/AnecdoteTable/route.js
+++ backend/src/app/api/AnecdoteTable/route.js
@@ -2,6 +2,8 @@
 // import { request } from 'http';
 import { docClient } from '../../../dbconfig.js';
 const TABLE_NAME = 'AnecdoteTask';
+
+// list table
 export async function GET() {
   const params = {
     TableName: TABLE_NAME,
@@ -14,31 +16,51 @@ export async function GET() {
   }
 }
 
+//delete table
+//! remember to check if request is actually needed
 export async function DELETE(request) {
   const { id } = await request.json();
   if (!id) return Response.error('id is requried');
-  return new Response(`Todo ${id} deleted `);
+
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      TaskId: id,
+    },
+  };
+  try {
+    const res = await docClient.delete(params).promise();
+    return Response.json(`Id: ${id} has been deleted`);
+  } catch {
+    return Response.error('Internal Server Error');
+  }
 }
-// export async function put() {
 
-//   const { AnectdoteId, status, createdTime, lastUpdatedTime, wordList, s3Url } = req.query;
-//   const params = {
-//     TableName: 'AnecdoteTask',
-//     Item: {
-//       // AnectdoteId,
-//       // status,
-//       // createdTime,
-//       // lastUpdatedTime,
-//       // wordList,
-//       // s3Url,
-//       test: "update from the backend"
-//   };
+//! what if the items passed by frontend is partially exsist
+export async function PUT(request) {
+  const { AnectdoteId, status, createdTime, lastUpdatedTime, wordList, s3Url } =
+    await request.json();
 
-//   try {
-//     const res = await docClient.put(params),promise();
-//   }
-// }
-// }
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      TaskId: AnectdoteId,
+      status,
+      createdTime,
+      lastUpdatedTime,
+      wordList,
+      s3Url,
+    },
+  };
+
+  try {
+    const res = await docClient.put(params).promise();
+    return Response.json(`id: ${AnectdoteId} has been put in to the dynamodb table ${TABLE_NAME}`);
+    return Response.json(`sucess`);
+  } catch {
+    return Response.error('Internal Server Error');
+  }
+}
 
 // res.setHeader('Access-Control-Allow-Origin', '*');
 // res.setHeader('Access-Control-Allow-Methods', '*');
