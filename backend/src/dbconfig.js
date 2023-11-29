74e0fe2a9ddff868d394c792425b7b1b910592bc
Anne <anneliu49@gmail.com>
2023-11-29 13:03:01 -0500

    vercel environment variable config test

--- backend/src/dbconfig.js
+++ backend/src/dbconfig.js
@@ -2,11 +2,9 @@ const TABLE_NAME = 'AnecdoteTask';
 const S3_BUCKET_NAME = 'anecdote-web';
 
 const AWS = require('aws-sdk');
-// const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
-// const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 AWS.config.update({ region: 'us-east-1' });
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const s3 = new AWS.S3();
-
+console.log('docClient and s3 works here');
 export { docClient, TABLE_NAME, s3, S3_BUCKET_NAME };
