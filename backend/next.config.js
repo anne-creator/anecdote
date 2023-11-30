3c3d9166fbf76fbdc7480769a1425331a2b1823f
Anne <anneliu49@gmail.com>
2023-11-30 00:32:09 -0500

    [Fix Deployment] Store credentials as environment variables in next.config.js

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // env: {
-  //   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-  //   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  // },
-};
 
-module.exports = nextConfig;
+module.exports = {
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
