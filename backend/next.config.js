1c136dec32ba11327250cff034f5d4764af9290f
Anne <anneliu49@gmail.com>
2023-11-30 00:44:26 -0500

    [Fix Vercel] Use environment variables that are not reserved by Vercel

--- backend/next.config.js
+++ backend/next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   env: {
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    MY_AWS_ACCESS_KEY: process.env.MY_AWS_ACCESS_KEY,
+    MY_AWS_SECRET_KEY: process.env.MY_AWS_SECRET_KEY,
   },
 };
--- backend/src/dbconfig.js
+++ backend/src/dbconfig.js
@@ -4,8 +4,8 @@ const S3_BUCKET_NAME = 'anecdote-web';
 const AWS = require('aws-sdk');
 AWS.config.update({
   region: 'us-east-1',
-  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
-  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+  aws_access_key_id: process.env.MY_AWS_ACCESS_KEY,
+  aws_secret_access_key: process.env.MY_AWS_SECRET_KEY,
 });
 console.log(process.env.AWS_ACCESS_KEY_ID);
 const docClient = new AWS.DynamoDB.DocumentClient();
