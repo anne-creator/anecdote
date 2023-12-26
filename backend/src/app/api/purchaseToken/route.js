ed5d68ee74bcbbb3792aa825e3430c606c1ebef4
Anne <anneliu49@gmail.com>
2023-12-26 12:29:59 -0500

    [Stripe] Install Stripe and add purchase token backend API
    * Created Stripe account and validated a simple payment exercise via Stripe console logs.

---
+++ backend/src/app/api/purchaseToken/route.js
@@ -0,0 +1,29 @@
+import Stripe from 'stripe';
+
+const test_secret_key =
+  'sk_test_51ORdFyGTkm7su90ROlXdSPi0j5Mz4Ip3Upih6Mm6GSw50tFVFrkq0wlZFgFSwcwsjxixwA3mYYvR7dLYmrj0Agh9004Pcf36Yp';
+const stripe = new Stripe(test_secret_key);
+
+export async function GET(request) {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Use price ID as the input, price IDs are generated
+          // from the Stripe dashboard when creating a product
+          price: 'price_1OReBZGTkm7su90RtCyIFhb4',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `http://localhost:3000/api/purchaseToken?success=true`,
+      cancel_url: `http://localhost:3000/api/purchaseToken?canceled=true`,
+      automatic_tax: { enabled: true },
+    });
+    // return Response.redirect({ url: session.url, status: 303 });
+    return Response.json('success');
+  } catch (error) {
+    console.log('error!');
+    return Response.error('Internal Server Error');
+  }
+}
