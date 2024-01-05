00ad836d0f659c1c180164835b913b21484d2171
Anne <anneliu49@gmail.com>
2024-01-05 14:56:34 -0500

    [real strip key testing]

--- frontend/src/app/(main)/purchaseToken/page.js
+++ frontend/src/app/(main)/purchaseToken/page.js
@@ -7,10 +7,10 @@ import { useRouter } from 'next/navigation';
 
 export default function purchaseToken() {
   const router = useRouter();
-  const test_publishable_key =
+  const publishable_key =
     // 'pk_test_51ORdFyGTkm7su90R4Okd6YJG8yDNMfmcWf50B2ITaF1rnVGL4evAisCcJcl52OAjKkhO9ZgfsLuzr3nmDUv9PYDm007zOGkjrK';
     'pk_live_51ORdFyGTkm7su90RFyBqXe3eqdHF8Uqk0dxM1QX7W3ZVXFyyunMEzc2MQp2oG1UNJUwmg0CWuDmVZkkXtBnWncN400dauPYKn'; //real key
-  const stripePromise = loadStripe(test_publishable_key);
+  const stripePromise = loadStripe(publishable_key);
 
   React.useEffect(() => {
     // Check to see if this is a redirect back from Checkout
